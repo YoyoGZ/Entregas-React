d54ea819f83a2addcb9ffd9ab47518b9078a56b7
YOYO1 <otrogithu>
2023-05-16 16:56:52 -0300

    1ra Subida del Proyecto Nuevo Repo

---
+++ src/components/Items/ItemCount/ItemCount.jsx
@@ -0,0 +1,28 @@
+import Flex from "./Flex"
+import { useState } from "react"
+
+function ItemCount ({ stock }) {
+  const [count, setCount] = useState(1);
+
+  function handleSuma () {
+    if (count < stock) setCount(count+1);
+  }
+
+  function handleRest () {
+    if (count > 1) setCount(count-1);
+  }
+  
+  return (
+    <div className="itemCount">
+        <div>
+          <Flex>
+            <button className="btn-countMin font-xl" onClick={handleRest}>-</button>
+            <p>{count}</p>
+            <button className="btn-countMas font-xl" onClick={handleSuma}>+</button>
+          </Flex>  
+        </div>
+    </div>
+  )
+}
+
+export default ItemCount
--- src/components/navBar/navBar.jsx
+++ src/components/navBar/navBar.jsx
@@ -1,35 +1,29 @@
-import CartWidget from "../cartWidget/cart";
-import "./navBar.css"
+import CartWidget from "../cartWidget/CartWidget";
+import "../../helpers/Styles.css"
 const NavBar = () => {
     return (
-            <nav className= "navBar">
-                <h2>The White Rabbit</h2>
-                <div className="btnNav cart">
-                    <ul>
-                        <li className="btnNav">
-                            <button>Nosotros</button>
+            <nav className= "navBar itemNav">
+                <a href="/#" className="logo"><h1 className="h1">The White Rabbit</h1></a>
+                    <ul className="itemNav">
+                        <li className="itemNav">
+                            <a href="/#">Nosotros</a>
                         </li>
-                        <li className="btnNav">
-                            <button>Nuestro Catalogo</button>
+                        <li className="itemNav">
+                            <a href="/#">Nuestro Catalogo</a>
                         </li>
-                        <li className="btnNav">
-                            <button>Mi cuenta</button>
+                        <li className="itemNav">
+                            <a href="/#">Mi Cuenta</a>
                         </li>
-                        <li className="btnNav">
-                            <button>Contacto</button>
+                        <li className="itemNav">
+                            <a href="/#">Favoritos</a>
                         </li>
-                        <li className="btnNav">
-                            <button>Favoritos</button>
-                        </li>
-                        <li className="btnNav">
-                            <button>Busqueda por Titulo</button>
+                        <li className="itemNav">
+                            <a href="/#">Contacto</a>
                         </li>
                         <div className="cart">
                             <CartWidget />
                         </div>
-
                     </ul>
-                </div>
             </nav>
         )
     };
